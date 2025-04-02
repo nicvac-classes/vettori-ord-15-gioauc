52d2f935256951b5718621d3dc38d15eab26813c
gioauc <giorgio.auciello@marconibari.edu.it>
2025-04-02 15:11:15 +0000

    CONSEGNA

--- main.cpp
+++ main.cpp
@@ -1,11 +1,47 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    
+    int i, j, n;
+    string s;
+    double t;
+    bool scambio;
+    cout << "Quanti studenti ci sono in una classe?" << endl;
+    cin >> n;
+    double p[n];
+    double voti[n];
+    string nomi[n];
+    i = 0;
+    while (i < n) {
+        cout << "Nome studente" << endl;
+        cin >> nomi[i];
+        cout << "Voto studente" << endl;
+        cin >> voti[i];
+        i = i + 1;
+    }
+    i = 0;
+    scambio = true;
+    while (i <= n - 1 && scambio) {
+        j = 0;
+        scambio = false;
+        while (i <= n - 2 - i) {
+            if (voti[j] > voti[j + 1]) {
+                t = voti[i];
+                voti[i] = voti[j + 1];
+                voti[j + 1] = t;
+                s = nomi[j];
+                nomi[j] = nomi[j + 1];
+                nomi[j + 1] = s;
+                scambio = true;
+            }
+            j = j + 1;
+        }
+        i = i + 1;
+    }
+    i = 0;
+    while (i < n) {
+        cout << nomi[i] << " " << p[i] << endl;
+        i = i + 1;
+    }
+    return 0;
 }
-
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
